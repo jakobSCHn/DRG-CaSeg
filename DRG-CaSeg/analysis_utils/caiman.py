fix: refined data normalization

DRG-CaSeg/analysis_utils/caiman.py
@@ -23,7 +23,9 @@ def extract_cnmfe_results(
     masks = A_dense.reshape(dims + (-1,), order="F").transpose(2, 0, 1)
     bin_masks = masks > 0
 
-    return bin_masks, traces
+    labels = np.arange(len(bin_masks)).tolist()
+
+    return bin_masks, traces, labels
 
 
 def get_default_params(
@@ -121,7 +123,7 @@ def run_cnmfe(
     mov: cm.movie,
     temp_path = None,
     cluster = None,
-    n_processes: int = 1,
+    n_processes: int = 2,
     **params,   
     ):
 
@@ -150,7 +152,7 @@ def run_cnmfe(
         
         cnmfe_model.fit(images)
 
-        masks, traces = extract_cnmfe_results(
+        masks, traces, labels = extract_cnmfe_results(
             model=cnmfe_model,
             dims=dims,
             )
@@ -158,6 +160,7 @@ def run_cnmfe(
         return {
             "masks": masks,
             "traces": traces,
+            "labels": labels,
         }
 
 

DRG-CaSeg/analysis_utils/ica.py
@@ -311,32 +311,23 @@ def extract_rois_and_traces(
             std_val = np.std(component_img)
             binary_mask = np.abs(component_img - mean_val) > (z_thresh * std_val)
 
-            structure = np.array(
-                [[0, 1, 0],
-                 [1, 1, 1],
-                 [0, 1, 0]]
-            )
+            structure = np.ones((3, 3))
             cleaned_mask = binary_opening(binary_mask, structure=structure)
+            cleaned_mask = binary_closing(cleaned_mask, structure=structure)
 
-            labeled_array, num_features = label(binary_mask)
-            blob_labels = np.arange(1, num_features + 1)
-            blob_sizes = ndi_sum(cleaned_mask, labeled_array, index=blob_labels)
-            good_blob_labels = blob_labels[(blob_sizes >= min_size) & 
-                                        (blob_sizes <= max_size)]
-            cleaned_mask = np.isin(labeled_array, good_blob_labels)
-
-            cleaned_mask = binary_closing(cleaned_mask, structure=structure).astype(int)
-            cleaned_mask = binary_opening(cleaned_mask, structure=structure)
-
+            labeled_array, num_features = label(cleaned_mask)
             
-            # Re-label the component_mask to separate any non-contiguous blobs
-            labeled_rois, num_rois = label(cleaned_mask)
-            if num_rois == 0:
+            if num_features == 0:
                 continue
+
             blob_labels = np.arange(1, num_features + 1)
             blob_sizes = ndi_sum(cleaned_mask, labeled_array, index=blob_labels)
+            
             good_blob_labels = blob_labels[(blob_sizes >= min_size) & 
                                            (blob_sizes <= max_size)]
+            
+            if good_blob_labels.size == 0:
+                continue
             component_mask = np.isin(labeled_array, good_blob_labels)
             labeled_rois, num_rois = label(component_mask)
 

DRG-CaSeg/data_utils/wrangler.py
@@ -60,7 +60,21 @@ def scale_video(
 
     #throw a type error if it's not a cm.movie, as metadata will be lost
     if not isinstance(mov, cm.movie):
-        TypeError("wrong type")
+        raise TypeError(f"cm.movie objects has been cast to {type(mov)} during preprocessing.")
+
+    return mov
+
+def norm_video(
+    mov: cm.movie,
+    ):
+    time_means = np.mean(mov, axis=0)
+    mov = (mov / (time_means + 1e-6)) - 1
+
+    frame_means = np.mean(mov, axis=(1,2), keepdims=True)
+    mov = mov - frame_means
+
+    if not isinstance(mov, cm.movie):
+        raise TypeError(f"cm.movie objects has been cast to {type(mov)} during preprocessing.")
 
     return mov
 